c71dbbb76bfe72ab23d7a761aab7857d52fa34d1
Alfahad <alfahadsaifi786@gmail.com>
2023-07-14 19:46:39 +0530

    uploading the completed tic tac toe flutter project.

---
+++ android/app/src/main/kotlin/com/alfahad/flutter/tic_tac_toe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alfahad.flutter.tic_tac_toe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = const Color(0xFF2F2F2F);
+  static Color secondaryColor = const Color(0xFF64FFDA);
+  static Color accentColor = const Color.fromARGB(255, 105, 68, 13);
+}
